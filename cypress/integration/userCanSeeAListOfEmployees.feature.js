1576b76d931d9da423cfa1fbdfd3abb27c782cfd
Thomas <thomas@craftacademy.se>
2020-02-24 16:21:25 +0100

    adds mock for api call

--- cypress/integration/userCanSeeAListOfEmployees.feature.js
+++ cypress/integration/userCanSeeAListOfEmployees.feature.js
@@ -1,4 +1,13 @@
 describe('Display a list of employees', () => {
+
+  before(()=>{
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'https://reqres.in/api/users?per_page=4',
+      response: 'fixture:employeeDataMock.json'
+    })
+  })
   it('with a header', () => {
     cy.visit('http://localhost:3000')
     cy.get('section[name="header"]').should('contain', 'Employee list')
